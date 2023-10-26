6e7306b2b477682f383eead7ad65e34622a8b87b
Melina Gutierrez <melina.gutierrez@ucb.edu.bo>
2023-10-26 12:57:01 -0400

    añadir permisos manifest

--- android/app/src/main/kotlin/com/example/guide_form/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/guide_form/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.guide_form
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterActivity() {
 }
